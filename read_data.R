b55581fc64cb315d675e503a6585bfaa4208395c
Floyd Everest <floyd.everest@monash.edu>
2024-02-06 13:56:52 +1100

    Moved data cleaning process to a new file

---
+++ read_data.R
@@ -0,0 +1,30 @@
+box::use(
+  dplyr[select, mutate, n, rename],
+  lubridate[ymd_hms, hours, force_tz],
+  readr[read_csv],
+  vroom[...]
+)
+
+# Load datasets
+wind <- readxl::read_excel(
+  "data/SunCable-30 year Time Series-Wind energy.xlsx",
+  sheet = 1,
+  skip = 9,
+  col_names = c("datetime", "year", "windspeed", "wind_energy")
+) |>
+  mutate( # Impose assumed date and time - ~^*^~"cleaning"~^*^~
+    date = ymd_hms("1993-01-01 10:00:00", tz = "Australia/Darwin") + hours(seq(0, n() - 1))
+  ) |>
+  select(-c(datetime, windspeed, year))
+
+
+solar <- read_csv(
+  "data/SunCable-15 year Time Series-Solar energy-570_bi_6.csv",
+  col_types = cols_only(
+    col_skip(),
+    col_datetime(format = "%d/%m/%Y %H:%M"),
+    col_guess()
+  )
+) |>
+  rename(date = "Date and time", solar_energy = "Energy produced [kWh]") |>
+  mutate(date = force_tz(date, "Australia/Darwin"))
