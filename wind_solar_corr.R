708d0a18dac9b63a5ab59fc5a424566058a02fac
Floyd Everest <floyd.everest@monash.edu>
2024-02-06 13:59:42 +1100

    Added some modelling boilerplate

---
+++ wind_solar_corr.R
@@ -0,0 +1,21 @@
+box::use(lib/read_data[wind, solar])
+
+library(tidyverse)
+library(sarima)
+
+ws <- merge(wind, solar) |> as_tibble()
+
+# Check cross-correlation between solar and wind
+stats::ccf(ws$solar_energy, ws$wind_energy, lag.max = 48)
+
+# Check autocorrelations
+stats::acf(ws$solar_energy, lag.max = 48)
+stats::acf(ws$wind_energy, lag.max = 48)
+
+## AirPassengers example
+## fit the classic airline model using arima()
+ap.arima <- arima(ws$wind_energy, order = c(0,1,1), seasonal = c(0,1,1))
+
+forecast::auto.arima(diff(ws$wind_energy, 24))
+
+diff(ws$wind_energy, 24) |> hist()
