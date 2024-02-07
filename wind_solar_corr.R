096bfdd198c57635d6a2b03eee858ccfde6eb03a
Floyd Everest <floyd.everest@monash.edu>
2024-02-07 12:27:09 +1100

    moved data merge into read_data.R

--- wind_solar_corr.R
+++ wind_solar_corr.R
@@ -1,21 +1,34 @@
-box::use(./read_data[wind, solar])
+box::use(./read_data[ws])
 
 library(tidyverse)
-library(sarima)
+library(forecast)
 
-ws <- merge(wind, solar) |> as_tibble()
+# Check daily cross-correlation between solar and wind
+stats::ccf(ws$solar_energy, ws$wind_energy, lag.max = 24)
+# Check monthly cross-correlation between solar and wind
+ws_monthly <- ws |>
+  group_by(month = floor_date(date, "month")) |>
+  summarize(
+    mean_solar_energy = mean(solar_energy),
+    mean_wind_energy = mean(wind_energy)
+  )
+stats::ccf(
+  ws_monthly$mean_solar_energy,
+  ws_monthly$mean_wind_energy,
+  lag.max = 24
+)
 
-# Check cross-correlation between solar and wind
-stats::ccf(ws$solar_energy, ws$wind_energy, lag.max = 48)
-
-# Check autocorrelations
-stats::acf(ws$solar_energy, lag.max = 48)
+# Check daily autocorrelations
+stats::acf(ws$solar_energy, lag.max = 365*24)
 stats::acf(ws$wind_energy, lag.max = 48)
 
-## AirPassengers example
-## fit the classic airline model using arima()
-ap.arima <- arima(ws$wind_energy, order = c(0,1,1), seasonal = c(0,1,1))
+## fit arima to wind energy
+wind.arima <- forecast::auto.arima(
+  ws$wind_energy,
+  order = c(0, 1, 1),
+  seasonal = c(0, 1, 1)
+)
 
-forecast::auto.arima(diff(ws$wind_energy, 24))
+forecast::auto.arima(ws$wind_energy, d = 24, D = 365 * 24)
 
 diff(ws$wind_energy, 24) |> hist()
